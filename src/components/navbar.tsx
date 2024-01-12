40f44f283a010b7f2f787d346c8321143db14e43
AchrafGarai <32675431+AchrafGarai@users.noreply.github.com>
2024-01-12 14:52:27 +0100

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,13 @@ function Navbar() {
 	return (
 		<div className="p-4 px-8 border-b flex flex-row items-center">
 			<div className="flex-grow w-full">
-				<Image src={"/next.svg"} width={80} height={56} alt="Next" />
+				<Image
+					src={"/next.svg"}
+					width={80}
+					height={56}
+					alt="Next"
+					className="dark:invert"
+				/>
 			</div>
 			<ModeToggle />
 		</div>
